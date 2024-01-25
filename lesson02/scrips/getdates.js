da7077cecc428e2dbc77e20eb37d26e6031deff2
wladij <joew.candelario@gmail.com>
2024-01-25 02:06:35 -0500

    lesson 2 finished

---
+++ lesson02/scrips/getdates.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const currentYear = new Date().getFullYear();
+    document.getElementById("current-year").innerText = currentYear;
+
+    
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").innerText = "Last Modified: " + lastModified;
+});
